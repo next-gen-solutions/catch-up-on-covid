1d817c65cd201f7599c7a1565c64e04b4c7ae407
yulduz <yulduz83@gmail.com>
2021-01-26 16:57:27 -0600

    add Samuels code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $("#search").on("click", function (e) {
   e.preventDefault();
   var country = $("#country-dropdown option:selected").text();
 
-  // display COVID-19 news for a given country
+  displayStatsForGivenCountry (country);
   displayNewsForCountry(country);
   displayChart(country);
 });
@@ -69,12 +69,77 @@ var displayNewsForCountry = (fullCountryName) => {
     });
 };
 
+var displayChart = (countryName) => {
+  var iframeEl = $("#chart iframe");
+  var countryCovidHandledBest = "US"; //TODO: @yulduz to make this retrieved from API
+  var countryCovidHandledWorst = "Italy"; //TODO: @yulduz to make this retrieved from API
+  iframeEl[0].attributes.src.nodeValue = `https://covid19chart.org/#/?bare=1&include=${countryCovidHandledWorst}%3B${countryName}%3B${countryCovidHandledBest}%3B&scale=linear&start=1%2F1%2F21&top=0&domain=&theme=dark&advanced=1`;
+};
 
-var displayChart = (countryName)=>{
-  var iframeEl = $('#chart iframe');
-  var countryCovidHandledBest = 'US'; //TODO: @yulduz to make this retrieved from API
-  var countryCovidHandledWorst = 'Italy'; //TODO: @yulduz to make this retrieved from API
-  iframeEl[0].attributes.src.nodeValue = `https://covid19chart.org/#/?bare=1&include=${countryCovidHandledWorst}%3B${countryName}%3B${countryCovidHandledBest}%3B&scale=linear&start=1%2F1%2F21&top=0&domain=&theme=dark&advanced=1`
-}
+var displayStatsForGivenCountry = (country) => {
+  // make a get request to url
+  var api =
+    "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=" +
+    country;
+  console.log(api);
+  fetch(api, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "5320d39b26msh9d1c12db50862d2p1c0e8ajsn0cf78c7c1705",
+      "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+    },
+  })
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        //call display function
+        response.json().then(function (data) {
+          //displays results
+          populateCovidStatsContent(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to GitHub");
+    });
+};
 
-displayNewsForCountry('US');
+var populateCovidStatsContent = (data) => {
+  //creating elements
+  var errorMessage = document.createElement("h2");
+  var countryName = document.createElement("h1");
+  var lastChecked = document.createElement("h1");
+  var currentCases = document.createElement("h1");
+  var currentDeaths = document.createElement("h1");
+  var currentRecoveries = document.createElement("h1");
+
+  if (data.data.location === "Global") {
+    errorMessage.innerHTML =
+      "This country is not yet supported by our program, please have a look at the global stats!";
+    errorMessage.setAttribute("class", "error");
+    countryName.innerHTML = data.data.location;
+    lastChecked.innerHTML = "updated on " + moment().format("LLL");
+    currentDeaths.innerHTML = " Current Deaths: " + data.data.deaths;
+    currentCases.innerHTML = "Current Cases: " + data.data.confirmed;
+    currentRecoveries.innerHTML = "Recovered Cases: " + data.data.recovered;
+  } else {
+    errorMessage.innerHTML = "";
+    countryName.innerHTML = data.data.location;
+    lastChecked.innerHTML = "updated on " + moment().format("LLL");
+    currentDeaths.innerHTML = " Current Deaths: " + data.data.deaths;
+    currentCases.innerHTML = "Current Cases: " + data.data.confirmed;
+    currentRecoveries.innerHTML = "Recovered Cases: " + data.data.recovered;
+  }
+  //append information
+
+  info.appendChild(errorMessage);
+  info.appendChild(countryName);
+  info.appendChild(lastChecked);
+  info.appendChild(currentCases);
+  info.appendChild(currentDeaths);
+  info.appendChild(currentRecoveries);
+};
+
+displayNewsForCountry("US");
